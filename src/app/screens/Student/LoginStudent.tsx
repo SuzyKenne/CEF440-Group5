98136915fa06931f84c6b9ff582d7da0b8af20bc
Mariekedjeunou <133589785+Mariekedjeunou@users.noreply.github.com>
2024-07-02 16:13:10 +0200

    perf(login): student login form added

--- src/app/screens/Student/LoginStudent.tsx
+++ src/app/screens/Student/LoginStudent.tsx
@@ -3,38 +3,90 @@ import React from "react";
 import { ThemedView } from "@/components/ThemedView";
 import { ThemedText } from "@/components/ThemedText";
 import CustomButton from "@/components/Atoms/Buttons/CustomButton";
-import { useRouter } from "expo-router";
+import { Link, useRouter } from "expo-router";
 import { Colors } from "@/constants/Colors";
-import Input from "@/components/Atoms/Form/Input";
+import Input from "@/components/Atoms/Form/Input";import { useForm, Controller, FieldValues } from "react-hook-form";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const LoginStudent = ({}) => {
   const router = useRouter();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({ mode: "onSubmit" });
+
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+    alert("submitted");
+    router.push("../LoginStudent.tsx");
+  };
   return (
     <ThemedView style={styles.container}>
       <Image
         source={require("../../../../assets/images/logo.png")}
-        style={styles.logo}
+        style={{ width: 200, height: 90 }}
         resizeMode="contain"
       />
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title" style={styles.title}>
-          Login
-        </ThemedText>
-        <Input
-          label="Matricule:"
-          onChangeText={() => {}}
-          placeholder="Enter Matricule"
+      <ThemedText type="title" style={styles.title}>
+        Login
+      </ThemedText>
+      
+        <SafeAreaView style={styles.form}>
+        <Controller
+          control={control}
+          name="Matricule"
+          render={({ field: { onChange, value } }) => (
+            <Input
+            value={value}
+            onChangeText={(val) => onChange(val)}
+                label="Matricule:"
+                placeholder="Enter Matricule"
+              />
+          )}
+          rules={{
+            required: {
+              value: true,
+              message: "Field is required!",
+            },
+          }}
         />
-        <Input
-          label="Password:"
-          onChangeText={() => {}}
-          placeholder="Enter Password"
+        <Controller
+          control={control}
+          name="Matricule"
+          render={({ field: { onChange, value } }) => (
+            <Input
+            value={value}
+            label="Password:"
+            onChangeText={(val) => onChange(val)}
+                placeholder="Enter Password"
+                secureTextEntry= {true}
+                iconName="eye"
+                size={25}
+              />
+          )}
+          rules={{
+            required: {
+              value: true,
+              message: "Field is required!",
+            },
+          }}
         />
-        <ThemedText type="title" style={styles.paragraph}>
-          Forgot Password?
+      <Link href={"/"} style={styles.pwd}>
+          <ThemedText type="link">Forgot Password?</ThemedText>
+        </Link>
+        <ThemedView style={styles.button}>
+      <ThemedText type="default" style={styles.signup}>Don't have an acccount? 
+        <ThemedText type="link">
+          <Link href={"./CreateAccountStudent"}> Sign Up</Link>
         </ThemedText>
-      </ThemedView>
-      <CustomButton title="Login" onPress={() => router.push("../")} />
+        </ThemedText>
+          <ThemedView style={{ marginLeft: 40 }}>
+            <CustomButton title="Login" onPress={handleSubmit(onSubmit)} />
+          </ThemedView>
+        </ThemedView>
+        </SafeAreaView>
     </ThemedView>
   );
 };
@@ -43,35 +95,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column",
-    margin: 20,
-    backgroundColor: "none",
-    gap: 50,
-  },
-  logo: {
-    width: "30%",
-    height: "30%",
-    alignSelf: "center",
-  },
-  titleContainer: {
-    backgroundColor: "none",
+    alignItems: "center",
+    justifyContent: "space-around",
   },
   title: {
-    margin: 15,
-    fontSize: 50,
-    fontWeight: "bold",
-    color: Colors.colorPrimary,
+    marginLeft: -200,
+    marginTop: 100,
+    color: Colors.colorPrimary, fontSize: 50,
   },
-  paragraph: {
+  form: {
+    width: "100%",
+  },
+  pwd: {
     fontSize: 15,
     color: Colors.colorPrimary,
     marginTop: 10,
+    marginRight: 30,
     alignSelf: "flex-end",
   },
-  pheye: {
-    flex: 1,
-    color: "black",
-    padding: 10,
+  button: {
+    marginTop: "50%",
   },
+  signup: {
+    textAlign:"center",
+  }
 });
 
 export default LoginStudent;
