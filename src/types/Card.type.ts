9d6b162346533b37503bc7f94f561830df4dc5fa
Aubin SIAHA <aubinsiaha9@gmail.com>
2024-07-03 12:09:50 -0700

    feat(component): the general card component
    this component allow us to make use of it as many times as we want, for notifications, courses,
    profiles, attendance, etc.

--- src/types/Card.type.ts
+++ src/types/Card.type.ts
@@ -1,7 +1,22 @@
 import React from "react";
+import { Style } from "react-native-paper/lib/typescript/components/List/utils";
 
 export type CardProps = {
   title: string;
   onPress: string;
   image: string;
 };
+export type CardGeneralProps = {
+  title: string;
+  content: string;
+  image?: string;
+  icon?: "notifications-sharp" | "person-sharp" | "book-sharp";
+  time?: string;
+  color?: Style;
+  bgColor?: Style;
+  status?: "present" | "absent";
+  nberPresence?: string;
+  letter?: string;
+};
+//     | "person-chalkboard"
+//     | "check-square";
