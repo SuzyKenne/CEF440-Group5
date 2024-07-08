8ac155266b68a379fd18450d1fb034f80d610452
SuzyKenne <133641382+SuzyKenne@users.noreply.github.com>
2024-07-08 13:56:35 +0000

    Delete AdminAttendEase directory
    ok

--- AdminAttendEase/src/screens/Help.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Sidebar from "../reusable component/Sidebar";
-import Navbar from "../reusable component/Navbar";
-const Help = () => {
-  return <div>Helps</div>;
-};
-
-export default Help;
